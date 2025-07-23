910d3a764d895449bdba3716e5ad42d48f5ce756
Harry <hg456wilkos@gmail.com>
2025-07-23 14:53:08 +0100

    update function

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,4 +1,7 @@
 function update --wraps=eos-update --description 'alias update=eos-update'
-  eos-update $argv
-        
+    eos-update $argv
+    sudo paccache -r
+    sudo pacman -Rns $(pacman -Qtdq)
+    sudo journalctl --vacuum-time=7d
+    sudo pacman -Sc --noconfirm
 end
